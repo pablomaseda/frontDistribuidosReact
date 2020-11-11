c37c8f7812c8fd122d5bf1706ce2caa28d6c0574
pablomaseda <pablomaseda@gmail.com>
2020-11-10 23:57:47 -0300

    Update store.js
    cambios en middleware

--- src/store.js
+++ src/store.js
@@ -4,14 +4,14 @@ import rootReducer from "./reducer";
 
 const initialState = {};
 
-const middleware = [thunk];
+//const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)//...middleware),
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
